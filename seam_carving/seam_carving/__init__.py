1f6b8bdec13a9d10cca16f1c8c6b3ae0150690b8
Nils <nilsvonnorsinski@gmail.com>
2019-04-03 00:29:04 +0200

    added documentation

--- seam_carving/seam_carving/__init__.py
+++
@@ -1,1 +0,0 @@
-from seam_carving import seamCarving
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='seamCarving',
+    version='',
+    packages=['seam_carving'],
+    url='',
+    license='',
+    author='Nils von Norsinski',
+    author_email='',
+    description='implements seam carving',
+    install_requires=['matplotlib', 'numpy', 'imageio', 'scipy']
+)
